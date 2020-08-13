25cf8a5fcefaae6e54a553439d4df3eeb611c273
Blakeando <52489043+Blakeando@users.noreply.github.com>
2020-08-14 00:54:30 +1000

    yeah whatever

--- OnaniCore/models.py
+++ OnaniCore/models.py
@@ -2,7 +2,7 @@
 # @Author: Blakeando
 # @Date:   2020-08-13 18:11:40
 # @Last Modified by:   Blakeando
-# @Last Modified time: 2020-08-13 22:56:47
+# @Last Modified time: 2020-08-14 00:54:22
 import logging
 
 log = logging.getLogger(__name__)
@@ -32,17 +32,28 @@ class Tag:
 
     def edit_alias(self, mode: str = "add", alias: str = None):
         if alias is None:
-            raise ValueError("Cannot pass no alias.")
+            raise ValueError("Alias is missing.")
         if mode == "add":
             self.aliases.append(alias)
         elif mode == "remove":
             self.aliases.remove(alias)
         # need to do extra db stuff here
 
+    def __str__(self):
+        return self.tag_string
+
 
 class Post:
     """
     Onani Post Object
     """
-    def __init__(self, ):
+
+    def __init__(self, db, post_data: dict):
+        self._db = db
+        self.file_url = post_data.get("file_url")
+        self.thumb_url = post_data.get("thumb_url")
+        self.tags = [Tag(self._db, x) for x in (post_data.get("tags") or list())]
+        self.meta = post_data.get("meta")
+
+    def add_tags(self, tags: list):
         pass
--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # @Author: Blakeando
 # @Date:   2020-08-12 15:52:51
 # @Last Modified by:   Blakeando
-# @Last Modified time: 2020-08-13 23:28:48
+# @Last Modified time: 2020-08-14 00:10:21
 
 import logging
 import os
@@ -14,7 +14,9 @@ from flask_sockets import Sockets
 import OnaniCore
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = b"\xd2\xc0\xe1\x00$\x06\x19\xef"
+app.config[
+    "SECRET_KEY"
+] = b"\xd2\xc0\xe1\x00$\x06\x19\xef"  # Temporary; Change to config generated one
 onaniDB = OnaniCore.DatabaseController(
     pymongo.MongoClient("mongodb://localhost:27017/")
 )
