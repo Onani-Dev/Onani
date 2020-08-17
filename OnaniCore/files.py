ab7ff65287521e7f7366aeeada977762c5d1a742
Blakeando <52489043+Blakeando@users.noreply.github.com>
2020-08-18 09:44:48 +1000

    started file controller will do more after class

--- OnaniCore/files.py
+++ OnaniCore/files.py
@@ -2,9 +2,11 @@
 # @Author: Blakeando
 # @Date:   2020-08-15 23:31:53
 # @Last Modified by:   Blakeando
-# @Last Modified time: 2020-08-17 20:27:48
+# @Last Modified time: 2020-08-18 09:41:46
 
+import hashlib
 import os
+from io import BytesIO
 
 
 class FileController(object):
@@ -12,8 +14,20 @@ class FileController(object):
     File Controller for Onani (Might be temporary idk will need to make one for amazon s3 or something)
     """
 
-    __slots__ = "location"
+    __slots__ = ("location", "DLController")
 
     def __init__(self, location: str):
+        if not os.path.isdir(location):
+            raise ValueError("Path does not exist.")
         self.location = location
 
+    def save_file(self, filebytes: bytes, filename: str = None):
+        pass
+
+    # INTERNAL FUNCTIONS
+    def _get_md5(self, file: BytesIO):
+        hash_md5 = hashlib.md5()
+        with file as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        return hash_md5.hexdigest()
