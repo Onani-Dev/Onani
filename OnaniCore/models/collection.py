d1d93672696fde85e60fd1006e1b8a7077383c62
Blakeando <52489043+Blakeando@users.noreply.github.com>
2020-08-23 00:10:41 +1000

    html stuff

--- OnaniCore/models/collection.py
+++ OnaniCore/models/collection.py
@@ -2,6 +2,43 @@
 # @Author: Blakeando
 # @Date:   2020-08-22 01:03:56
 # @Last Modified by:   Blakeando
-# @Last Modified time: 2020-08-22 01:04:56
+# @Last Modified time: 2020-08-22 18:44:22
 
-# TODO #16 Collections 😥
+import logging
+from datetime import datetime
+from typing import List
+from aenum import Enum, MultiValue
+
+log = logging.getLogger(__name__)
+
+
+class CollectionStatus(Enum):
+    """
+    Status for collections
+    """
+
+    _init_ = "value string"
+    _settings_ = MultiValue
+
+    BANNED = 0, "Banned"
+    PENDING = 1, "Pending"
+    ACCEPTED = 2, "Accepted"
+
+    def __int__(self):
+        return self.value
+
+
+# Iwant die
+class Collection(object):
+
+    __slots__ = (
+        "_db",
+        "id",
+        "title",
+        "description",
+        "posts",
+        "status" "created_at",
+        "creator",
+        "rating",
+    )
+
