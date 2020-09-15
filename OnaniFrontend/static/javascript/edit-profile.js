1eb50654a02d38ee834a361e47adccf97f0cf681
Blakeando <52489043+Blakeando@users.noreply.github.com>
2020-09-15 17:42:38 +1000

    more profile stuff

--- OnaniFrontend/static/javascript/edit-profile.js
+++ OnaniFrontend/static/javascript/edit-profile.js
@@ -2,23 +2,22 @@
  * @Author: Blakeando
  * @Date:   2020-09-14 00:47:49
  * @Last Modified by:   Blakeando
- * @Last Modified time: 2020-09-14 15:05:38
+ * @Last Modified time: 2020-09-15 17:30:40
  */
 'use strict';
-let editMode = false;
-const editButton = document.getElementById("profile-edit-button");
 
-document.addEventListener('DOMContentLoaded', init, false);
-function init() {
 
-  editButton.addEventListener("click", function () {
-    if (!editMode) {
-      editMode = true;
-      editButton.innerHTML = "Save Changes";
-    }
-    else {
-      editMode = false;
-      editButton.innerHTML = "Edit Profile";
-    }
-  });
-}
+
+$uploadCrop = $('#profile-settings-profile-picture').croppie({
+  enableExif: true,
+  viewport: {
+    width: 150,
+    height: 150,
+    type: 'square'
+  },
+  boundary: {
+    width: 300,
+    height: 300
+  }
+});
+
