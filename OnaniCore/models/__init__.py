6b746616d4db149b9206c09458b0864beea66bb7
Blakeando <52489043+Blakeando@users.noreply.github.com>
2020-09-22 10:32:03 +1000

    oops

--- OnaniCore/models/__init__.py
+++ OnaniCore/models/__init__.py
@@ -2,11 +2,11 @@
 # @Author: Blakeando
 # @Date:   2020-08-22 01:54:07
 # @Last Modified by:   Blakeando
-# @Last Modified time: 2020-09-09 03:16:53
+# @Last Modified time: 2020-09-22 10:31:51
 
 from .ban import Ban
 from .commentary import Commentary
 from .note import Note
 from .post import Post, PostData, PostFile, PostRating, PostStatus
 from .tag import Tag, TagType
-from .user import User, UserPermissions, UserSettings
+from .user import User, UserPermissions, UserPlatforms, UserSettings
