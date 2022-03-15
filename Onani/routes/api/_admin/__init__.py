c4939574f768b9532ce3b24684a207f2e868c9ea
Yourredyknowwhoitisss <26773027+kero3009destiny@users.noreply.github.com>
2022-03-15 21:35:01 +0300

    added meta tags and opensearch
    opensearch should be configured later with the production onani hostname.

--- Onani/routes/api/_admin/__init__.py
+++ Onani/routes/api/_admin/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Author: kapsikkum
 # @Date:   2022-03-09 03:37:06
-# @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-03-09 03:39:25
+# @Last Modified by:   dirt3009
+# @Last Modified time: 2022-03-15 17:56:41
 from .. import admin_api, db
 from . import index
