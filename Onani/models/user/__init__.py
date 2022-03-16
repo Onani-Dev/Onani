d2fa827b9bfa62860ca2eda6e81ded0647d1595b
Blakeando <52489043+Blakeando@users.noreply.github.com>
2022-03-17 02:31:05 +1100

    incomplete permissions

--- Onani/models/user/__init__.py
+++ Onani/models/user/__init__.py
@@ -2,10 +2,10 @@
 # @Author: kapsikkum
 # @Date:   2022-03-13 00:38:28
 # @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-03-13 01:14:08
+# @Last Modified time: 2022-03-17 00:07:16
 from .. import db
 from .role import UserRoles
 from .settings import UserSettings
-
+from .permissions import UserPermissions
 
 from .model import User
---
+++ Onani/models/user/permissions.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @Author: kapsikkum
+# @Date:   2022-03-16 15:16:39
+# @Last Modified by:   kapsikkum
+# @Last Modified time: 2022-03-16 20:23:58
+from enum import Flag, auto
+from functools import reduce
+from operator import or_ as _or_
+
+
+class UserPermissions(Flag):
+    CAN_POST = auto()
+    CAN_FLAG = auto()
+    PRIORITY_FLAG = auto()
+    EDIT_POSTS = auto()
+
+    CAN_COMMENT = auto()
+    LOCK_COMMENTS = auto()
+    DELETE_COMMENTS = auto()
+    MERGE_POSTS = auto()
+
+    BAN_USERS = auto()
+    EDIT_COLLECTIONS = auto()
+
+    @classmethod
+    def all(cls):
+        cls_name = cls.__name__
+        if not len(cls):
+            raise AttributeError("empty %s does not have an ALL value" % cls_name)
+        value = cls(reduce(_or_, cls))
+        cls._member_map_["ALL"] = value
+        return value
+
+    @classmethod
+    def none(cls):
+        return cls(0)
