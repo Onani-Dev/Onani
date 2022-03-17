4dfa5767b01ad85a517bc6d51ad1aa3d6edfd365
Yourredyknowwhoitisss <26773027+kero3009destiny@users.noreply.github.com>
2022-03-17 23:24:06 +0300

    News API actually works this time
    Need help writing DELETE method

--- Onani/routes/api/_admin/news.py
+++ Onani/routes/api/_admin/news.py
@@ -2,25 +2,30 @@
 # @Author: dirt3009
 # @Date:   2022-03-17 20:38:10
 # @Last Modified by:   dirt3009
-# @Last Modified time: 2022-03-17 21:18:08
+# @Last Modified time: 2022-03-17 23:23:31
 
 import datetime
 
 from flask import jsonify, request
 from flask_login import current_user
 from Onani.controllers import role_required
-from Onani.models import UserRoles
+from Onani.models import UserRoles, NewsPost, NewsPostSchema
 
 from . import admin_api, db
 from .. import make_api_response
 
-@admin_api.route("/news", methods=["POST"])
+
+@admin_api.route("/news", methods=["POST", "DELETE"])
 @role_required(role=UserRoles.ADMIN)
 def create_article():
-    data = request.json
-    uploader = db.Column(db.Integer, current_user.id)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    title = db.Column(db.String, data["title"])
-    content = db.Column(db.UnicodeText, data["content"])
-    db.session.commit()
-    return make_api_response()
+    if request.method == "POST":
+        data = request.json
+        article = NewsPost(
+            author=current_user.id,
+            title=data["title"],
+            content=data["content"],
+        )
+        article.save_to_db()
+        return make_api_response()
+    elif request.method == "DELETE":
+        return make_api_response(error="Not implemented yet.", code=501) # why does this not work properly when i add code=501 part?
