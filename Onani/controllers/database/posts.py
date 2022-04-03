2767d22786dd4af66e86cc489b24fbe5ba5076a8
Mattlau <37871312+Mattlau04@users.noreply.github.com>
2022-04-03 14:52:11 +0200

    Added format_tag() and parse_tags()

--- Onani/controllers/database/posts.py
+++ Onani/controllers/database/posts.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # @Author: kapsikkum
 # @Date:   2022-03-31 23:58:51
-# @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-04-01 01:51:25
+# @Last Modified by:   Mattlau04
+# @Last Modified time: 2022-04-03 14:51:11
+
 from cgi import FieldStorage
+from typing import List
 
 from flask import request
 from flask_login import current_user
@@ -12,6 +14,7 @@ from Onani.models import Post, PostComment, PostRating, Tag, User
 from PIL import UnidentifiedImageError
 
 from . import create_files, db
+from Onani.controllers.utils import startswith_min
 
 
 def create_comment(author: User, post: Post, content: str) -> PostComment:
@@ -34,6 +37,82 @@ def create_comment(author: User, post: Post, content: str) -> PostComment:
     db.session.commit()
 
     return comment
+
+
+def format_tag(tag: str) -> str:
+    """Takes in a string and formats it to be a tag's name"""
+    tag = tag.lower().strip()
+    if not tag:
+        return ''
+
+    if 32 < len(tag):
+        # Maybe display a warning to the user?
+        return ''
+
+    tag = tag.replace(" ", "_")
+    return tag
+
+
+def parse_tags(post: Post, tags: List[str]) -> List[Tag]:
+    """Parses a list of tags, and returns the list of tags to actually add to the post
+
+    Args:
+        post (Post): The post that the tags will be added to (used by meta tags)
+        tags (List[str]): a list of the tags to parse
+
+    Returns:
+        List[Tag]: The tags to add to the post
+    """    
+
+    taglist = []
+
+    for tag_str in tags:
+        # first we format the tag
+        tag_str = format_tag(tag_str)
+        if not tag_str:
+            # empty tag OR invalid tag
+            continue
+
+        # We handle special tags
+        new_tag_type = None
+        if ':' in tag_str: 
+            prefix, tag_no_prefix = tag_str.split(':', 1)
+            # Checks for meta tags
+            if prefix == 'pool' or startswith_min('collection', prefix, 3):
+                # TODO: Add the post to the pool
+                continue # don't add the tag to the post
+
+            if startswith_min('source', prefix, 3):
+                # TODO: add source validation
+                post.source = tag_no_prefix
+                continue # don't add the tag to the post
+            
+            # We check for types
+            for type in TagType:
+                if type == TagType.BANNED: # You can't make a tag "BANNED"
+                    continue
+                if startswith_min(type.name.lower(), prefix, min_len=3): # We don't want 1-2 char bc ambiguous
+                    # make the tag 'tag' of type 'type'
+                    new_tag_type = type
+                    break
+
+        # /!\ We should only reach here if the tag is not a metatag
+        # that shouldn't be added to the post
+
+        # Check if the tag exists and make it if not
+        tag = Tag.query.filter_by(name=tag_str).first()
+        if not tag:
+            # make it and add it to the session
+            tag = Tag(name=t, post_count=0)
+            db.session.add(tag)
+
+        if new_tag_type is not None:
+            tag.type = new_tag_type
+        
+        taglist.append(tag)
+            
+    db.session.commit()
+    return taglist
 
 
 def upload_post(form: UploadForm):
