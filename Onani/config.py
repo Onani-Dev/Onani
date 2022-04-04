277587b6c3e57c7af857e6ba17a90273b0dd0c9d
Blakeando <52489043+Blakeando@users.noreply.github.com>
2022-04-05 01:53:11 +1000

    fucked

--- Onani/config.py
+++ Onani/config.py
@@ -2,13 +2,13 @@
 # @Author: kapsikkum
 # @Date:   2022-03-01 16:12:35
 # @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-04-05 01:47:37
+# @Last Modified time: 2022-04-05 01:52:03
 import os
 
 # Flask Config
 STATIC_PATH = "/static/"
 SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
-TESTING = os.environ.get(bool("TESTING"), False)
+TESTING = bool(os.environ.get("TESTING", False))
 
 if SECRET_KEY == "dev":
     print(
