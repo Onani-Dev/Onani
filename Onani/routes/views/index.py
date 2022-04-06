5dc7b9e0f0f7727989bbe98779a865dd272e27b9
Blakeando <52489043+Blakeando@users.noreply.github.com>
2022-04-07 02:13:57 +1000

    IDK
    Co-authored-by: Yourredyknowwhoitisss <kero3009destiny@users.noreply.github.com>

--- Onani/routes/views/index.py
+++ Onani/routes/views/index.py
@@ -2,7 +2,7 @@
 # @Author: kapsikkum
 # @Date:   2022-03-09 02:59:30
 # @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-03-19 00:14:49
+# @Last Modified time: 2022-04-07 02:12:47
 from flask import render_template, request, redirect
 from Onani.models import Ban, Post, Tag
 
@@ -10,13 +10,8 @@ from . import main, db
 
 
 @main.route("/")
-# @main.route("/posts/")
 def posts():
     return redirect("/posts/")
-
-
-
-
 
     # # render the index template
     # return render_template(
