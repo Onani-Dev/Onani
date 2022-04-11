564c5f62bc71d6af594f63cfa28c607b1ee4ae2c
Blakeando <52489043+Blakeando@users.noreply.github.com>
2022-04-11 14:57:42 +1000

    Made generate.py more user friendly

--- generate.py
+++ generate.py
@@ -1,28 +1,42 @@
 # @Author: kapsikkum
 # @Date:   2022-03-06 20:54:49
 # @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-03-20 19:35:52
+# @Last Modified time: 2022-04-11 14:55:49
 
 import os
 import secrets
 import string
 
+# Check if the file exsits, if it does we don't need to do anything.
+if os.path.exists("./.env"):
+    raise FileExistsError(".env File already exists!")
+
+# Generate random values for secret key and database password
 DB_PASSWORD = "".join(
     secrets.choice(string.ascii_letters + string.digits) for _ in range(20)
 )
 FLASK_SECRET_KEY = os.urandom(24)
 
-if os.path.exists("./.env"):
-    raise Exception(".env File already exists!")
+# Prompt user for captcha keys
+RECAPTCHA_PUBLIC_KEY = input("ReCaptcha Public Key: ")
+RECAPTCHA_PRIVATE_KEY = input("ReCaptcha Private Key: ")
 
+# Write to .env file
 with open(".env", "w") as w:
     w.write(f"DB_PASSWORD={DB_PASSWORD}\n")
     w.write(f"FLASK_SECRET_KEY={FLASK_SECRET_KEY}\n")
-    w.write("RECAPTCHA_PUBLIC_KEY=\n")
-    w.write("RECAPTCHA_PRIVATE_KEY=\n")
+    w.write(f"RECAPTCHA_PUBLIC_KEY={RECAPTCHA_PUBLIC_KEY}\n")
+    w.write(f"RECAPTCHA_PRIVATE_KEY={RECAPTCHA_PRIVATE_KEY}\n")
 
+# Print values
 print(
-    f"""Generated environment variables:
-DB_PASSWORD={DB_PASSWORD}  
-FLASK_SECRET_KEY={FLASK_SECRET_KEY}"""
+    f"""
+Generated environment variables:
+DB_PASSWORD={DB_PASSWORD}
+FLASK_SECRET_KEY={FLASK_SECRET_KEY}
+
+Inputted environment variables:
+RECAPTCHA_PUBLIC_KEY={RECAPTCHA_PUBLIC_KEY}
+RECAPTCHA_PRIVATE_KEY={RECAPTCHA_PRIVATE_KEY}
+"""
 )
