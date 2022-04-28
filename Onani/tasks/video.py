dd1aaba655fe5167d85d8cf04dda2b95757890d5
Yourredyknowwhoitisss <26773027+kero3009destiny@users.noreply.github.com>
2022-04-28 21:16:11 +0300

    nope.
    i have realized this is beyond my skillset and should be done with mr blake anderson.

--- Onani/tasks/video.py
+++ Onani/tasks/video.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 # @Author: kapsikkum
 # @Date:   2022-04-26 00:35:31
-# @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-04-29 02:18:58
+# @Last Modified by:   dirt3009
+# @Last Modified time: 2022-04-28 21:16:05
 
 from celery import shared_task
+import ffmpeg
 
 
 @shared_task
 def test(string):
     return "".join(reversed(string))
+
+
+@shared_task
+def encode(inname, outname):
+    stream = ffmpeg.input(inname)
+    stream = ffmpeg.output(
+        stream,
+        outname,
+        **{"crf": 50, "preset": "fast", "cpu-used": -8, "deadline": "realtime"}
+    )
+    ffmpeg.run(stream)
