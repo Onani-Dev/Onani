0b4b3ed3043c9abbab2166d80566ef39b1cc1b67
Mattlau04 <37871312+Mattlau04@users.noreply.github.com>
2022-04-30 05:19:58 +0200

    circular import fix

--- Onani/tasks/database.py
+++ Onani/tasks/database.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 # @Author: kapsikkum
 # @Date:   2022-04-29 02:27:38
-# @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-04-29 02:42:58
+# @Last Modified by:   Mattlau04
+# @Last Modified time: 2022-04-30 04:41:23
 
+from typing import Optional
 from celery import shared_task
 
 from . import db
 
 
 @shared_task
-def database_test(user):
-    return str(user.id)
+def database_test(user_id: int) -> Optional[str]:
+    from Onani.models import User
+
+    user = User.query.filter_by(id=user_id).first()
+    return str(user.username)
