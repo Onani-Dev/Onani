6f093a41028faa86585b5700d3b5637ca844cf3f
kapsikkum <kapsikkum@onani.xyz>
2022-07-06 07:43:38 +0000

    codevid-19

--- Onani/importers/__init__.py
+++ Onani/importers/__init__.py
@@ -2,7 +2,7 @@
 # @Author: Mattlau04
 # @Date:   2022-05-01 01:34:41
 # @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-07-04 03:24:32
+# @Last Modified time: 2022-07-06 07:24:49
 
 from .. import db
 
@@ -11,7 +11,7 @@ from ._baseimporter import BaseImporter, IMPORTERS
 from ._utils import find_importer, get_post, save_imported_post
 
 # All importers need to be imported so they're initialised
-from . import rule34
+from . import rule34, danbooru
 
 # EXAMPLE:
 # print(get_post("https://rule34.xxx/index.php?page=post&s=view&id=6021167"))
--- Onani/importers/danbooru.py
+++ Onani/importers/danbooru.py
@@ -2,60 +2,57 @@
 # @Author: kapsikkum
 # @Date:   2022-05-14 07:38:57
 # @Last Modified by:   kapsikkum
-# @Last Modified time: 2022-06-27 03:19:26
+# @Last Modified time: 2022-07-06 07:27:55
 
 
-# from typing import TYPE_CHECKING, Optional
-# from urllib.parse import parse_qs, urlparse
+from typing import TYPE_CHECKING, Dict, Optional
 
-# import requests
-# from Onani.models import PostRating
+import requests
+from Onani.models import PostRating
 
-# from . import BaseImporter, ImportedPost
+from . import BaseImporter, ImportedPost
 
 
-# class Ruler34Importer(BaseImporter, URLs=["rule34.xxx", "hypnohub.net"]):
-#     """Importer for rule34.xxx, and all other sites running Gelbooru Beta 0.2"""
+class DanbooruImporter(BaseImporter, URLs=["danbooru.donmai.us"]):
+    """Importer for danbooru.donmai.us"""
 
-#     def _get_pid_from_url(self, url: str) -> Optional[str]:
-#         pid = parse_qs(urlparse(url).query).get("id")
-#         if not pid:
-#             # User might have given broken url where there is no id
-#             return None
-#         pid = pid[0]
-#         return pid
+    def _get_pid_from_url(self, url: str) -> Optional[str]:
+        pid = url.split("/")[-1]
+        if not pid.isdigit():
+            return None
+        return pid
 
-#     def normalize_url(self, url: str) -> str:
-#         pid = self._get_pid_from_url(url)
-#         if not pid:
-#             raise ValueError("URL is missing the id")
-#         return f"https://{self.URL}/index.php?page=post&s=view&id={pid}"
+    def _get_post_tags(self, json_data: Dict[str, str]):
+        tags = []
+        # There's gotta be a better way to do this right?
+        for tag_type in ("general", "character", "copyright", "artist"):
+            tags.extend(
+                [
+                    f"{tag_type}:{x}"
+                    for x in json_data[f"tag_string_{tag_type}"].split(" ")
+                ]
+            )
+        return tags
 
-#     def get_post(self, url: str) -> Optional[ImportedPost]:
-#         pid = self._get_pid_from_url(url)
-#         if not pid:
-#             return None
+    def normalize_url(self, url: str) -> str:
+        pid = self._get_pid_from_url(url)
+        if not pid:
+            raise ValueError("URL is missing the id")
+        return f"https://{self.URL}/posts/{pid}"
 
-#         r = requests.get(
-#             f"https://api.{self.URL}/index.php",
-#             params={
-#                 "page": "dapi",
-#                 "s": "post",
-#                 "q": "index",
-#                 "json": "1",
-#                 "id": pid,
-#             },
-#         )
+    def get_post(self, url: str) -> Optional[ImportedPost]:
+        pid = self._get_pid_from_url(url)
 
-#         # It returns nothing if there's no post, not even empty json
-#         if not r.text:
-#             return None
+        if not pid:
+            return None
 
-#         r = r.json()[0]
-#         return ImportedPost(
-#             tags=r["tags"].split(" "),
-#             sources=[self.normalize_url(url)],  # Rule34's API doesn't return source...
-#             file_urls=[r["file_url"]],
-#             description="",
-#             rating=PostRating.QUESTIONABLE,  # TODO CHANGE THESE TO THE ACTUAL VALUES
-#         )
+        r = requests.get(f"https://{self.URL}/posts/{pid}.json").json()
+
+        return ImportedPost(
+            imported_url=self.normalize_url(url),
+            tags=self._get_post_tags(r),
+            sources=[r["source"]],
+            file_url=r["file_url"],
+            description="",
+            rating=PostRating(r["rating"]),
+        )
